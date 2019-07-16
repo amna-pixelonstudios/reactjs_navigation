9740f5eab165bfe4d9bd4791aa01b1bd021dcf18
amna-pixelonstudios <48746957+amna-pixelonstudios@users.noreply.github.com>
2019-07-16 15:55:22 +0500

    done react-router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import './App.css';
+import './App.css'; 
+import {BrowserRouter, Route} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 // component in file
 function Navigation() {
   return (
-    <div className="header">
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">Career</a></li>
-        <li><a href="#">Services</a></li>
-      </ul>
-    </div>
+    <BrowserRouter>
+      <div className="header">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </div>
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/contact' component={Contact} />
+    </BrowserRouter>
   );
 }
 
-export default Navigation;
+export default Navigation; 
  
