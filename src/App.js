c2f461040740395cbb3f666318eb9b8685148cf7
amna-pixelonstudios <48746957+amna-pixelonstudios@users.noreply.github.com>
2019-07-16 16:10:41 +0500

    done with web-router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {BrowserRouter, Route} from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Services from './components/Services';
+import Career from './components/Career';
 
 // component in file
 function Navigation() {
@@ -14,11 +16,15 @@ function Navigation() {
           <li><a href="/">Home</a></li>
           <li><a href="/about">About</a></li>
           <li><a href="/contact">Contact</a></li>
+          <li><a href="/services">Services</a></li>
+          <li><a href="/career">Career</a></li>
         </ul>
       </div>
       <Route exact path='/' component={Home} />
       <Route path='/about' component={About} />
       <Route path='/contact' component={Contact} />
+      <Route path='/services' component={Services} />
+      <Route path='/career' component={Career} />
     </BrowserRouter>
   );
 }
---
+++ src/components/Career.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Career = ()=>{
+    return(
+        <div className="container">
+            <h1>Career....</h1>
+        </div>
+    );
+}
+
+export default Career;
